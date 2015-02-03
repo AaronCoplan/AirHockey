69c8c8562dac25b6eef76b3b491c4fcd27e61c0a
Aaron F Coplan <afcoplan@verizon.net>
2015-02-03 12:41:01 -0500

    Deleted a class which was only used for testing

--- Gannon Air Hockey/src/ackermanCoplanMuscianoAirHockey/FourPersonServer.java
+++ Gannon Air Hockey/src/ackermanCoplanMuscianoAirHockey/FourPersonServer.java
@@ -12,12 +12,12 @@ public class FourPersonServer {
 	private Socket clientSocket1, clientSocket2, clientSocket3;
 	private PrintWriter pw1, pw2, pw3;
 	private BufferedReader br1, br2, br3;
-	private String name, name1, name2, name3;
+	private String hostName, name1, name2, name3;
 	private final int TIMEOUT = 120 * 1000;
 	
 	
-	public FourPersonServer(String name){
-		this.name = name;
+	public FourPersonServer(String hostName){
+		this.hostName = hostName;
 	}
 	
 	public void startServer(){
@@ -34,7 +34,8 @@ public class FourPersonServer {
 			clientSocket1 = ss.accept();
 			br1 = new BufferedReader(new InputStreamReader(clientSocket1.getInputStream()));
 			pw1 = new PrintWriter(clientSocket1.getOutputStream(), true);
-			pw1.println(name);
+			pw1.println(1);
+			pw1.println(hostName);
 			name1 = br1.readLine();
 		}catch(Exception e){
 			e.printStackTrace();
@@ -46,7 +47,8 @@ public class FourPersonServer {
 			clientSocket2 = ss.accept();
 			br2 = new BufferedReader(new InputStreamReader(clientSocket2.getInputStream()));
 			pw2 = new PrintWriter(clientSocket2.getOutputStream(), true);
-			pw2.println(name);
+			pw2.println(2);
+			pw2.println(hostName);
 			name2 = br2.readLine();
 		}catch(Exception e){
 			e.printStackTrace();
@@ -58,14 +60,15 @@ public class FourPersonServer {
 			clientSocket3 = ss.accept();
 			br3 = new BufferedReader(new InputStreamReader(clientSocket3.getInputStream()));
 			pw3 = new PrintWriter(clientSocket3.getOutputStream(), true);
-			pw3.println(name);
+			pw3.println(3);
+			pw3.println(hostName);
 			name3 = br3.readLine();
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 	}
 	
-	public String getUserName(){return name;}
+	public String getUserName(){return hostName;}
 	public String getOpponent1Name(){return name1;}
 	public String getOpponent2Name(){return name2;}
 	public String getOpponent3Name(){return name3;}
