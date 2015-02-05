ba2586c2160ec12a4d7d84db081cfd78dcd9ed96
ackerman.adam <ackattack1111@yahoo.com>
2015-02-05 08:36:16 -0500

    altered the max speed a little bit

--- Gannon Air Hockey/src/ackermanCoplanMuscianoAirHockey/ComputerGAME.java
+++ Gannon Air Hockey/src/ackermanCoplanMuscianoAirHockey/ComputerGAME.java
@@ -328,12 +328,12 @@ public class ComputerGAME {
 			puckDIRY = (puck.getCY() - userPaddle.getCY()) / (Math.sqrt(((puck.getCX() - userPaddle.getCX()) * (puck.getCX() - userPaddle.getCX())) + ((puck.getCY() - userPaddle.getCY()) * (puck.getCY() - userPaddle.getCY()))));
 
 			//transfers paddle speed to the puck
-			if(userPaddleSpeed < 5){ //if the user paddle speed is less than 5, add it to the current puck speed
+			if(userPaddleSpeed < 4){ //if the user paddle speed is less than 5, add it to the current puck speed
 				puckSpeed += userPaddleSpeed;
-			}else if(userPaddleSpeed <= 15){ //otherwise, assign it as the puck speed
+			}else if(userPaddleSpeed <= 13){ //otherwise, assign it as the puck speed
 				puckSpeed = userPaddleSpeed;
 			}else{ //this ensures the max puck speed is 15
-				puckSpeed = 15;
+				puckSpeed = 13;
 			}
 			puckStep(); //calls the puck step method so that the puck does not get stuck in the paddle
 
