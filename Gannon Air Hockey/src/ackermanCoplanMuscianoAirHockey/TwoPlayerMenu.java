a726873ff10442701167fd410279aae64dd45ab0
Aaron F Coplan <afcoplan@verizon.net>
2015-02-13 14:25:09 -0500

    Update

--- Gannon Air Hockey/src/ackermanCoplanMuscianoAirHockey/TwoPlayerMenu.java
+++ Gannon Air Hockey/src/ackermanCoplanMuscianoAirHockey/TwoPlayerMenu.java
@@ -83,7 +83,7 @@ public class TwoPlayerMenu {
 			if(e.getSource().equals(host)){ //if they click host, do the following
 				
 				String yourName = (String)JOptionPane.showInputDialog(null, "Enter your name (15 characters max):", "Name", JOptionPane.QUESTION_MESSAGE);
-				yourName = yourName.substring(0, 15);
+				if(yourName.length() > 15){yourName = yourName.substring(0, 15);}
 				server = new TwoPersonServer(yourName);
 				
 				//change to connecting screen
@@ -117,7 +117,7 @@ public class TwoPlayerMenu {
 			}else if(e.getSource().equals(join)){ //if they click join, do the following
 				
 				String yourName = (String)JOptionPane.showInputDialog(null, "Enter your name (15 characters max):", "Name", JOptionPane.QUESTION_MESSAGE);
-				yourName = yourName.substring(0, 15);
+				if(yourName.length() > 15){yourName = yourName.substring(0, 15);}
 				client = new TwoPersonClient(IPs, yourName);
 				
 				//change to connecting screen
