8a6f6e6dfcf44a5288add920527951b182cb6074
Aaron F Coplan <afcoplan@verizon.net>
2015-02-17 14:11:13 -0500

    length

--- Gannon Air Hockey/src/ackermanCoplanMuscianoAirHockey/TwoPlayerMenu.java
+++ Gannon Air Hockey/src/ackermanCoplanMuscianoAirHockey/TwoPlayerMenu.java
@@ -82,8 +82,8 @@ public class TwoPlayerMenu {
 			
 			if(e.getSource().equals(host)){ //if they click host, do the following
 				
-				String yourName = (String)JOptionPane.showInputDialog(null, "Enter your name (10 characters max):", "Name", JOptionPane.QUESTION_MESSAGE);
-				if(yourName.length() > 10){yourName = yourName.substring(0, 10);}
+				String yourName = (String)JOptionPane.showInputDialog(null, "Enter your name (9 characters max):", "Name", JOptionPane.QUESTION_MESSAGE);
+				if(yourName.length() > 9){yourName = yourName.substring(0, 9);}
 				server = new TwoPersonServer(yourName);
 				
 				//change to connecting screen
@@ -116,8 +116,8 @@ public class TwoPlayerMenu {
 				
 			}else if(e.getSource().equals(join)){ //if they click join, do the following
 				
-				String yourName = (String)JOptionPane.showInputDialog(null, "Enter your name (10 characters max):", "Name", JOptionPane.QUESTION_MESSAGE);
-				if(yourName.length() > 10){yourName = yourName.substring(0, 10);}
+				String yourName = (String)JOptionPane.showInputDialog(null, "Enter your name (9 characters max):", "Name", JOptionPane.QUESTION_MESSAGE);
+				if(yourName.length() > 9){yourName = yourName.substring(0, 9);}
 				client = new TwoPersonClient(IPs, yourName);
 				
 				//change to connecting screen
